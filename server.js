c8558096a436aed9545ca5902c2790b7c30ea8ad
Wonjohn Choi <wonjohn.choi@gmail.com>
2016-04-20 23:16:04 -0700

    Added login check with mongodb.

--- server.js
+++ server.js
@@ -1,19 +1,59 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 var app = express();
-var fs = require("fs");
 
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-   });
+var mongojs = require('mongojs')
+var db = mongojs('localhost:27017/solidware_mini_db', ['users'])
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 })
 
+// check if given username and password pair is stored in solidware_mini_db:users
+login_success = function(username, password, callback) {
+    db.users.find(function(err, docs) {
+        if (err != null) {
+            console.log('In solidware_mini_db:users, an error occurred:', err);
+        } else if (docs != null) {
+            success = false;
+            name = '';
+            docs.forEach(function(doc) {
+                // TODO(wonjohn): in real life, password shouldn't be stored in db
+                // in plain text as in here..
+                if (doc['username'] == username
+                    && doc['password'] == password) {
+                    console.log("login success!");
+                    success = true;
+                    name = doc['name'];
+                }
+            });
+            callback(success, name);
+        }
+    });
+}
+
+app.post('/login', function(req, res){
+    // TODO(wonjohn): handle case in which req.body does not have login or password.
+    username = req.body['login'];
+    password = req.body['password'];
+    console.log('POST username: ' + username);
+    console.log('POST password: ' + password);
+
+    login_success(username, password, function(success, name) {
+        if (success) {
+            res.
+        } else {
+            res.send('username or password was wrong.');
+        }
+    });
+});
+
+
 var server = app.listen(8081, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
-
+  console.log('listening http://127.0.0.1:8081/');
 })
